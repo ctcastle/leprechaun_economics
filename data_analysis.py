160c8874bb8df28ca71946facf37e4480bb79214
William Watson <williamwatson@Williams-Laptop.local>
2025-06-18 20:44:36 -0600

    added recent analysis to data_analysis.py (everything except the pivot tables).

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import os 
 
 # Data cleaning and merging
 
@@ -102,6 +104,16 @@ def check_years(years_list, start_year, end_year):
         raise KeyError("Invalid year range. Years must be in proper range, and start year must come before end year.")
     return
 
+
+def describe_all(df):
+    decade_list = [10 * (int(year) // 10) for year in df.columns]
+    decade_list = [str(year) for year in decade_list]
+    decade_list = pd.Series(decade_list).unique()
+
+    for metric in df.index.get_level_values('Metric').unique():
+        print(f"*** {metric} General Stats ***")
+        print(df.loc[metric][decade_list].describe())
+
 def check_country(country_list, new_country):
 
     new_country = new_country.strip()
@@ -109,6 +121,15 @@ def check_country(country_list, new_country):
     if not (new_country in country_list):
         raise KeyError("Country not in list. Try again.")
     return
+
+
+def plotter(df, country_list, year_range, metric):
+    idx = pd.IndexSlice
+    sub_df = df.loc[idx[metric, :,country_list],idx[year_range[0]:year_range[-1]]]
+    sub_df.transpose().plot() 
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    plt.show()
+
 
 
 def get_user_input(combined_data):
@@ -172,19 +193,6 @@ def get_user_input(combined_data):
 
 
 
-def describe(df, user_input):
-
-    # nation GDP histogram for start and end years, after that cound do a groupby to do stats on each country category.
-    # categories could be something like over 10TN (superpowers), between 1-10TN (highly developed / large), 
-    
-    # among the countries chosen, rank most recent year for all metrics
-    # show average year over year GDP growth percentage
-    # for start and end years, range
-
-    # for country in country list
-        # average gdp growth % over time
-
-
 
 
     return
@@ -195,33 +203,86 @@ def describe(df, user_input):
 def main():
 
 
-
-    total_gdp = pd.read_csv("total_gdp_us_inflation_adjusted.csv")
+    print(os.getcwd())
+    total_gdp = pd.read_csv("leprechaun_economics/total_gdp_us_inflation_adjusted.csv")
     total_gdp = total_gdp.set_index('country')
 
-    gdp_percap = pd.read_csv("gdppercapita_us_inflation_adjusted.csv")
+    gdp_percap = pd.read_csv("leprechaun_economics/gdppercapita_us_inflation_adjusted.csv")
     gdp_percap = gdp_percap.set_index('country')
-    gini = pd.read_csv("gini.csv")
+    gini = pd.read_csv("leprechaun_economics/gini.csv")
     gini = gini.set_index('country')
 
-    # make copy of original raw data
-    total_gdp_copy = total_gdp.copy()
-    gdp_percap_copy = gdp_percap.copy()
-    gini_copy = gini.copy()
+    # make sorted copy of original raw data
+    total_gdp_copy = total_gdp.sort_index().copy()
+    gdp_percap_copy = gdp_percap.sort_index().copy()
+    gini_copy = gini.sort_index().copy()
         
     # parse suffixes and convert values from strings to floats
     parse_gdp_values(total_gdp_copy)
     parse_gdp_values(gdp_percap_copy)
+    # After parsing, force all columns to be numeric
+    
+    # Forces columns to numeric data instead of objects ( Code written by ChatGPT )
+    year_cols = total_gdp_copy.columns.tolist()
+    total_gdp_copy[year_cols] = total_gdp_copy[year_cols].apply(pd.to_numeric, errors='coerce')
+    year_cols = gdp_percap_copy.columns.tolist()
+    gdp_percap_copy[year_cols] = gdp_percap_copy[year_cols].apply(pd.to_numeric, errors='coerce')
 
+    # Performs inner join on datasets and converts to MultiIndex Dataframe
     combined_data = df_index_inner_join(total_gdp_copy, gdp_percap_copy, gini_copy, 'Total GDP', 'GDP Per Capita', 'Gini Coeff')
     combined_data.index.names = ['Metric', 'Country']
-    print(combined_data.shape)
+    
+    # Data Cleaning
+    print("***************** Cleaning Data *******************")
+    print("Original data size:", combined_data.shape)
+    data_cleaned = drop_countries_by_nan(combined_data, 0.5)
+    print("New data size:", data_cleaned.shape)
+    print("***************************************************")
 
-    data_nans_dropped = drop_countries_by_nan(combined_data, 0.5)
-    print(data_nans_dropped.shape)
+    # General Stats
+    print("***************** General Stats *******************")
+    describe_all(data_cleaned)
 
-    user_input_test = get_user_input(combined_data)
-    print(user_input_test)
+    # Data Processing
+    print("***************** Data Processing *****************")
+    # calculate gini dollars ( defined as GDP Per Capita * (1 - gini) )
+    new_df = data_cleaned.loc['GDP Per Capita'] * (1 - data_cleaned.loc['Gini Coeff']/100)
+    new_df.round()
+    new_df.index = pd.MultiIndex.from_product([['Gini Dollars'], new_df.index], names=['Metric', 'Country'])
+
+    print(data_cleaned.shape)
+    print(new_df.shape)
+    new_data_cleaned = pd.concat([data_cleaned, new_df])
+
+    # Add new 'Classification' Index Level
+    economic_weightclass = pd.cut(new_data_cleaned.loc['Total GDP',:]['2023'], bins=[0, 1e11, 1e12, 1e13, 1e14],labels=['Lightweight','Middleweight', 'Heavyweight', 'Superpower'])
+    income_class = pd.cut(new_data_cleaned.loc['GDP Per Capita',:]['2023'], bins=[0, 1135, 4465, 13845, 40000, 300000],labels=['Low Income','Lower Middle Income', 'Upper Middle Income', 'High Income', 'Ultra High Income'])
+    equality_class = pd.cut(new_data_cleaned.loc['Gini Coeff',:]['2023'], bins=[0, 30, 35, 40, 45, 50, 100],labels=['Very Low Inequality','Low Inequality', 'Low-Moderate Inequality', 'Moderate Inequality', 'Moderate-High Inequality', 'High Inequality'])
+    livability_class = pd.cut(new_data_cleaned.loc['Gini Dollars',:]['2023'], bins=[0, 1135, 4465, 13845, 30000, 300000],labels=['Low Livability','Low-Moderate Livability', 'Moderate Livability', 'Moderate-High Livability', 'High Livability'])
+
+    classification = pd.concat([economic_weightclass, income_class, equality_class, livability_class])
+    metrics = new_data_cleaned.index.get_level_values('Metric')
+    countries = new_data_cleaned.index.get_level_values('Country')
+
+    # Use the classification series you created
+    new_index = pd.MultiIndex.from_arrays([metrics, classification.values, countries], names=['Metric', 'Classification', 'Country'])
+
+    # Assign to the DataFrame
+    new_data_cleaned.index = new_index
+    print(new_data_cleaned.shape)
+    describe_all(new_data_cleaned)
+
+    # user_input = get_user_input(data_cleaned)
+    # print(user_input)
+
+    test_input = (['Canada', 'Germany', 'Ireland', 'USA'], ['1960', '2023'])
+
+    plotter(new_data_cleaned,test_input[0],test_input[1],'Total GDP')
+    plotter(new_data_cleaned,test_input[0],test_input[1],'GDP Per Capita')
+    plotter(new_data_cleaned,test_input[0],test_input[1],'Gini Coeff')
+    plotter(new_data_cleaned,test_input[0],test_input[1],'Gini Dollars')
+
+
 
 
 
